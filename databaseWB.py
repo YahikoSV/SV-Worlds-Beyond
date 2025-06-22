2edfdcd01df812f33908e5c73ecd1186ee57407a
Yahiko_Daichi <42230158+YahikoSV@users.noreply.github.com>
2025-06-22 18:26:51 +0800

    uploaded files
    Created repository for SVWB

---
+++ databaseWB.py
@@ -0,0 +1,143 @@
+# -*- coding: utf-8 -*-
+"""
+SV Portal Database
+"""
+
+import requests
+import random as rand
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+
+#Used to get all card set info
+#initial_link = 'https://shadowverse-wb.com/en/deck/cardslist' 
+initial_link = 'https://shadowverse-wb.com/en/deck/cardslist/card/?card_id=10012110'
+source = requests.get(initial_link).text
+soup = bs(source, 'lxml')
+pretty_soup = soup.prettify()
+
+a = soup.find_all('div', attrs={"class":"card-pack-list-wrapper"})
+
+card_box = soup.find_all('fieldset', attrs={"data-field":"card_set"})
+card_set=card_box[0].find_all('label')
+
+
+card_set_num_list = []
+card_set_name_list = []
+for card_set_spec in card_set:
+    number = card_set_spec.input['value']
+    name = card_set_spec.span.text
+    card_set_num_list.append(number)
+    card_set_name_list.append(name)
+    
+card_set_df = pd.DataFrame({'Set_Number':card_set_num_list, 'Set_Name':card_set_name_list})    
+
+
+#Used to get all card infor pertaining to a cardset
+set_number = card_set_df['Set_Number'][1]
+set_name = card_set_df['Set_Name'][1]
+initial_link = 'https://shadowverse-portal.com/cards?lang=en' 
+added_fields = f'&card_set%5B%5D={set_number}&format=1'
+source = requests.get(initial_link + added_fields).text
+soup = bs(source, 'lxml')
+pretty_soup = soup.prettify()
+
+card_box = soup.find_all('a', attrs={"class":"el-card-visual-content"})
+card_href_list = []
+for card_box_spec in card_box:
+    card_href_list.append(card_box_spec['href'])
+
+
+set_num_list = []
+set_name_list = []
+name_list = []
+trait_list = []
+class_list = []
+type_list = []
+rarity_list = []
+vial_list = []
+flavor_list = []
+flavor_e_list = []
+skill_list = []
+skill_e_list = []
+atk_list = []
+atk_e_list = []
+life_list = []
+life_e_list = []
+
+for href_spec in card_href_list:
+    initial_link = 'https://shadowverse-portal.com'    
+    added_fields = href_spec + '?lang=en'
+    chosen_card_number = href_spec.split('/')[2]
+    source = requests.get(initial_link + added_fields).text
+    soup = bs(source, 'lxml')
+    pretty_soup = soup.prettify()
+    
+    
+    
+    card_info = soup.find('ul', class_="card-info-content")
+    card_text = card_info.find_all('span')
+    flavors   = soup.find_all('p', class_="card-content-description") 
+    
+    set_num_list.append(set_number)
+    set_name_list.append(set_name)
+    name_list.append(soup.title.text.split(' |')[0])
+    trait_list.append(card_text[1].text.split('\r\n')[1])
+    class_list.append(card_text[3].text.split('\r\n')[1])
+    rarity_list.append(card_text[5].text.split('\r\n')[1])
+    vial_list.append(card_text[7].text.split('\r\n')[1])
+    flavor_list.append(flavors[0].text.split('\r\n')[1])
+    
+    if int(chosen_card_number[-4]) == 1: #follower
+        skill_txt = soup.find_all('p', class_="card-content-skill")        
+        if skill_txt[0].text == '\n':
+            skill_list.append('None')
+        else:
+            skill_list.append(str(skill_txt[0]).split('>',1)[1].split('</p>',1)[0].split('\r\n')[-2])            
+        if skill_txt[1].text == '\n':
+            skill_e_list.append('None')
+        else:
+            skill_e_list.append(str(skill_txt[1]).split('>',1)[1].split('</p>',1)[0].split('\r\n')[-2]) 
+        flavor_e_list.append(flavors[1].text.split('\r\n')[1])
+        atk_list.append(soup.find_all('p', class_="el-card-status is-atk")[0].text.split('\r\n')[1])  
+        atk_e_list.append(soup.find_all('p', class_="el-card-status is-atk")[1].text.split('\r\n')[1])
+        life_list.append(soup.find_all('p', class_="el-card-status is-life")[0].text.split('\r\n')[1])   
+        life_e_list.append(soup.find_all('p', class_="el-card-status is-life")[1].text.split('\r\n')[1]) 
+        type_list.append('Follower')    
+        
+    else: #non-follower
+        skill_list.append(str(soup.find_all('p', class_="card-content-skill")[0]).split('>',1)[1].split('</p>',1)[0].split('\r\n')[1])
+        skill_e_list.append('N/A')
+        flavor_e_list.append('N/A')
+        atk_list.append('N/A')  
+        atk_e_list.append('N/A')
+        life_list.append('N/A')   
+        life_e_list.append('N/A')
+        if int(chosen_card_number[-4]) == 4:
+            type_list.append('Spell')
+        elif int(chosen_card_number[-4]) == 3:
+            type_list.append('Ctd. Amulet')   
+        elif int(chosen_card_number[-4]) == 2:
+            type_list.append('Amulet')               
+        
+card_database_df = pd.DataFrame(
+    {'Set_Number':set_num_list, 
+     'Set_Name':set_name_list, 
+     'Card_Name':name_list,
+     'Card_Type':type_list,
+     'Class':class_list,
+     'Trait(s)':trait_list,     
+     'Rarity':rarity_list,
+     'Vials':vial_list,   
+     'Attack (Unevo)':atk_list, 
+     'Life (Unevo)':life_list,      
+     'Skill (Unevo)':skill_list,
+     'Attack (Evo)':atk_e_list, 
+     'Life (Evo)':life_e_list,      
+     'Skill (Evo)':skill_e_list,     
+     }
+    )  
+
+with pd.ExcelWriter("output.xlsx") as writer:
+    card_set_df.to_excel(writer, sheet_name='Set Index')
+    card_database_df.to_excel(writer, sheet_name='Card Database')
+  
---
+++ vials_critical_mass.py
@@ -0,0 +1,187 @@
+import random as rn
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import Counter
+
+#Prefix Code
+code_dict = {
+     'B' : 'Bronze'
+    ,'S' : 'Silver'
+    ,'G' : 'Gold'
+    ,'L' : 'Legendary'
+    ,'T' : 'Ticket'
+    }
+
+#Unique Cards per Rarity
+unique_dict = {
+     'Bronze'    : 45    
+    ,'Silver'    : 37
+    ,'Gold'      : 37
+    ,'Legendary' : 23
+    ,'Ticket'    : 4
+}
+
+#Rates per Rarity (Normal)
+rate_dict_n = {
+     'Bronze'    : 0.6744
+    ,'Silver'    : 0.250
+    ,'Gold'      : 0.060
+    ,'Legendary' : 0.015    
+    ,'Ticket'    : 0.0006
+}
+
+#Rates per Rarity (Silver+ Pity)
+rate_dict_s = {
+     'Bronze'    : 0
+    ,'Silver'    : 0.9244
+    ,'Gold'      : 0.060
+    ,'Legendary' : 0.015    
+    ,'Ticket'    : 0.0006
+}
+
+#Rates per Rarity (Legendary Pity)
+rate_dict_l = {
+     'Bronze'    : 0
+    ,'Silver'    : 0
+    ,'Gold'      : 0
+    ,'Legendary' : 1  
+    ,'Ticket'    : 0
+}
+
+#Vials liquefied per Rarity
+vials_liquefied_non = {
+     'Bronze'    : 10
+    ,'Silver'    : 20
+    ,'Gold'      : 200
+    ,'Legendary' : 1200  
+    ,'Ticket'    : 0
+}
+
+#Vials liquefied per Animated Rarity
+vials_liquefied_ani = {
+     'Bronze'    : 30
+    ,'Silver'    : 50
+    ,'Gold'      : 450
+    ,'Legendary' : 2500  
+    ,'Ticket'    : 0
+}
+
+rarity_list = list(unique_dict.keys())
+rate_animated = 0.08
+
+#Inputs
+simulations = 1000
+num_packs = 500
+cards_in_pack = 8
+num_pack_pity = 10
+
+#Vial System Inputs
+liquefy_priority = 'Normal' #Normal, Animated
+num_copies_restrict = 3  #Can't vial until reaching n copies
+
+#Plotting
+pack_increment = 10
+histogram_snapshot = 100
+histogram_bins = 20
+pack_increment_list = np.arange(pack_increment,num_packs + pack_increment, pack_increment)
+total_vials_list = np.zeros((simulations,int(num_packs/pack_increment)))
+vial_increment_list = np.zeros((simulations,int(num_packs/pack_increment)))
+
+def pull_card(rarity_list, rate_dict, u_dict, rate_animated):
+    for card_pack in range(0,cards_in_pack) :
+        random_rarity_num = rn.random() #gacha rarity
+        random_animated_num = rn.random()
+        if random_rarity_num < rate_dict['Bronze']:
+            card_rarity = rarity_list[0]
+        elif random_rarity_num < rate_dict['Bronze'] + rate_dict['Silver']:
+            card_rarity = rarity_list[1]
+        elif random_rarity_num < rate_dict['Bronze'] + rate_dict['Silver'] + rate_dict['Gold']:
+            card_rarity = rarity_list[2]
+        elif random_rarity_num < rate_dict['Bronze'] + rate_dict['Silver'] + rate_dict['Gold'] + rate_dict['Legendary']:
+            card_rarity = rarity_list[3]       
+        else:
+            card_rarity = rarity_list[4]
+        random_name = rn.randrange(0,u_dict[card_rarity]) #gacha name
+        random_animated = 'A' if random_animated_num < rate_animated else 'N'
+        card_id_w_ani = card_rarity[0]+str(random_name)+random_animated
+        card_id = card_rarity[0]+str(random_name)
+        return card_id, card_id_w_ani
+
+    
+def potential_vials(card_w_copies_over, count_cards, count_cards_w_ani, card_liquefy_1st_prefix, vials_liquefy_1st, vials_liquefy_2nd):
+    vials_gained = 0
+    for card_id in cards_w_copies_over:
+        excess = count_cards[card_id] - num_copies_restrict
+        min_excess_non_prio = min(excess,count_cards_w_ani[f'{card_id}{card_liquefy_1st_prefix}'])
+        vials_gained_card = vials_liquefy_1st[code_dict[card_id[0]]] * (min_excess_non_prio) + vials_liquefy_2nd[code_dict[card_id[0]]] * (excess - min_excess_non_prio)
+        vials_gained += vials_gained_card
+    return vials_gained or 0 #To not return None type
+
+
+
+for sim_num in range(0,simulations):
+    count_pack_pity = 0 #last pack (legendary)
+    count_card_pity = 0 #last card (silver+, legendary)
+    count_card_list = []
+    count_card_list_w_ani = []
+    new_vial_total = 0 
+    #Pulling Session
+    for pack in range(0,num_packs):
+        count_pack_pity += 1
+        for card in range(0,cards_in_pack):
+            count_card_pity += 1 
+            if count_pack_pity == num_pack_pity and card == cards_in_pack - 1: #Legendary: No leggo in 10 packs and is the 8th pack
+                card_id, card_id_w_ani = pull_card(rarity_list, rate_dict_l, unique_dict, rate_animated)
+            elif count_card_pity == cards_in_pack: #Silver+: No Silver+ and is the 8th pack
+                card_id, card_id_w_ani = pull_card(rarity_list, rate_dict_s, unique_dict, rate_animated)
+            else: #Normal
+                card_id, card_id_w_ani = pull_card(rarity_list, rate_dict_n, unique_dict, rate_animated)       
+            count_card_list.append(card_id) #add to cardlist
+            count_card_list_w_ani.append(card_id_w_ani) #add to cardlist
+            if card_id[0] in [rarity_list[3][0]]: #check if card is Legendary
+                count_pack_pity = 0
+            if card_id[0] in [i[0] for i in rarity_list[1:4]]: #check if card is Silver+
+                count_card_pity = 0                                           
+        count_card_pity = 0
+    
+    
+        if (pack + 1) % pack_increment == 0:
+            #Count your cards and vials
+            count_cards = Counter(count_card_list)   
+            count_cards_w_ani = Counter(count_card_list_w_ani)
+            current_vial_total = new_vial_total
+            #Vial Potential
+            cards_w_copies_over = [card_id for card_id, copies in count_cards.items() if copies > num_copies_restrict]
+            if liquefy_priority == 'Animated':
+                new_vial_total = potential_vials(cards_w_copies_over, count_cards, count_cards_w_ani, 'A', vials_liquefied_ani, vials_liquefied_non)
+            elif liquefy_priority == 'Normal':
+                new_vial_total = potential_vials(cards_w_copies_over, count_cards, count_cards_w_ani, 'N', vials_liquefied_non, vials_liquefied_ani)            
+            vials_gained_increment = new_vial_total - current_vial_total
+            vial_increment_list[sim_num][int(((pack+1)/pack_increment)-1)] = vials_gained_increment
+            total_vials_list[sim_num][int(((pack+1)/pack_increment)-1)] = new_vial_total
+            #print(pack+1, new_vial_total, current_vial_total, vials_gained_increment)
+
+        
+critical_mass = plt.plot(pack_increment_list,vial_increment_list[0])
+plt.title(f'Vials per {pack_increment} packs (1 player)')
+plt.show(critical_mass)
+
+total_mass = plt.plot(pack_increment_list,total_vials_list[0])
+plt.title('Vials total (1 player)')
+plt.show(total_mass)
+
+
+avg_vial_increment_list = [vial_increment_list[:,i].sum()/len(vial_increment_list[:,i]) for i in range(0,vial_increment_list.shape[1])]
+avg_total_vials_list =  [total_vials_list[:,i].sum()/len(total_vials_list[:,i]) for i in range(0,total_vials_list.shape[1])]
+
+hist_vials_per_player = plt.hist(total_vials_list[:,int((histogram_snapshot/pack_increment)-1)],histogram_bins) 
+plt.title(f'Vial distribuition of {simulations} players @{histogram_snapshot} packs')
+plt.show(hist_vials_per_player)
+
+avg_critical_mass = plt.plot(pack_increment_list,avg_vial_increment_list)
+plt.title(f'Average Vials per {pack_increment} packs (n={simulations})')
+plt.show(critical_mass)
+
+avg_total_mass = plt.plot(pack_increment_list,avg_total_vials_list)
+plt.title(f'Average Vials total (n={simulations})')
+plt.show(total_mass)
